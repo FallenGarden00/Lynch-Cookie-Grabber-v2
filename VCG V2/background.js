c8f63159971c73d6e9026738bc63d2fb58a05e98
Himothy <goobaweltz@gmail.com>
2024-08-18 04:47:56 -0400

    Delete VCG V2 directory

--- VCG V2/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "openForm") {
-    chrome.tabs.create({ url: "https://docs.google.com/forms/d/1qLBMhoKNG31PbdvakzINwqIGQMrZpjUzAPq4SwFEPdk/edit" });
-  }
-});
--- VCG V2/popup.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("begin").addEventListener("click", () => {
-  chrome.runtime.sendMessage({ action: "openForm" });
-});
